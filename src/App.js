f6d949305f5bcd0d57ee5b07d746b3e72140b5f6
gunwoongPark <bung1438@gmail.com>
2022-06-29 18:01:57 +0900

    현재 지도의 중심 좌표 출력 & 내 위치로 이동 버튼 구현

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ function App() {
   const mapContainer = useRef(null);
 
   const [map, setMap] = useState(null);
+  const [centerCoordinate, setCenterCoordinate] = useState('');
 
   // 지도 생성
   useEffect(() => {
@@ -18,23 +19,33 @@ function App() {
     // 지도 생성
     const map = new window.kakao.maps.Map(mapContainer.current, options);
 
+    // 축척 제한
+    map.setMinLevel(1);
+    map.setMaxLevel(12);
+
+    // 지도에 이벤트 등록
+    // 중심좌표 변경
+    window.kakao.maps.event.addListener(map, 'center_changed', () => {
+      setCenterCoordinate(`중심좌표의 위도 : ${map.getCenter().getLat()} 경도 ${map.getCenter().getLng()}`);
+    });
+
     // setState
     setMap(map);
   }, []);
 
-  useEffect(() => {
-    if (!!map) {
-      // 축척 제한
-      map.setMinLevel(1);
-      map.setMaxLevel(12);
-    }
-  }, [map]);
+  // function
+  const onClickMoveCurrentLocation = () => {
+    const moveLatLon = new window.kakao.maps.LatLng(37.506214, 127.053397);
+    map.panTo(moveLatLon);
+  };
 
   return (
     <div className="App">
       <div className="map-container" ref={mapContainer} />
       <button onClick={() => map.setLevel(map.getLevel() - 1)}>확대</button>
       <button onClick={() => map.setLevel(map.getLevel() + 1)}>축소</button>
+      <button onClick={onClickMoveCurrentLocation}>내 위치로 위동</button>
+      <p>{centerCoordinate && centerCoordinate}</p>
     </div>
   );
 }
